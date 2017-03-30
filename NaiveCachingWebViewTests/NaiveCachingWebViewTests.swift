28c0fe6c1e1c0ea3b73dc5738e10bdf9c425f839
guoc <guochen42@gmail.com>
2017-03-31 09:00:19 +1100

    Refactor testing, includes test-links, add autorelease in loop.

--- NaiveCachingWebViewTests/NaiveCachingWebViewTests.swift
+++ NaiveCachingWebViewTests/NaiveCachingWebViewTests.swift
@@ -13,20 +13,20 @@ import WebKit
 
 class NaiveCachingWebViewTests: FBSnapshotTestCase {
     
-    
-    let dispatchGroup = DispatchGroup()
-    
+    static private let testLinks: [String] = {
+        
+        let filePath = Bundle(for: NaiveCachingWebViewTests.self).path(forResource: "test-links", ofType: "txt")!
+        let fileContent = try! String(contentsOfFile: filePath, encoding: .utf8)
+        let links = fileContent.components(separatedBy: "\n")
+        return links
+    }()
+        
     let window = UIWindow(frame: UIScreen.main.bounds)
-    var webView: WKWebView!
     
     override func setUp() {
         super.setUp()
         
         URLCache.shared.removeAllCachedResponses()
-        
-        webView = WKWebView(frame: window.bounds)
-        webView.navigationDelegate = self
-        window.addSubview(webView)
     }
     
     override func tearDown() {
@@ -40,56 +40,104 @@ class NaiveCachingWebViewTests: FBSnapshotTestCase {
         
         let request = URLRequest(url: URL(string: "http://hackage.haskell.org/package/bytedump")!)
         
-        _ = webView.load(request)
-        waitWebViewLoadingFinished()
-        let nativeLoadingResult = image(forViewOrLayer: webView)
+        let nativeLoadingResult = syncLoad(request: request)
 
-        _ = webView.cachingLoad(request)
-        waitWebViewLoadingFinished()
-        let cachingLoadingResult = image(forViewOrLayer: webView)
+        let cachingLoadingResult = syncCachingLoad(request: request)
 
         FBSnapshotCompareReferenceImage(nativeLoadingResult, to: cachingLoadingResult, tolerance: 0)
     }
     
     func testCachingCorrection() {
         
-        let request = URLRequest(url: URL(string: "http://hackage.haskell.org/package/bytedump")!)
+//        let request = URLRequest(url: URL(string: "http://hackage.haskell.org/package/bytedump")!)
         
-        _ = webView.load(request)
-        waitWebViewLoadingFinished()
-        let nativeLoadingResult = image(forViewOrLayer: webView)
-        
-        let expectation = self.expectation(description: "Snapshots comparison finished")
-        _ = webView.cachingLoad(request, with: nil, cachingCompletionHanlder: {
-            _ = self.webView.cachingLoad(request) // cachingLoad again to load caches.
-            self.waitWebViewLoadingFinished()
-            let cachingLoadingResult = self.image(forViewOrLayer: self.webView)
-            self.FBSnapshotCompareReferenceImage(nativeLoadingResult, to: cachingLoadingResult, tolerance: 0)
-            expectation.fulfill()
-        })
-        waitWebViewLoadingFinished()
-        
-        wait(for: [expectation], timeout: 10)
+        for (index, link) in NaiveCachingWebViewTests.testLinks.enumerated() {
+            
+            autoreleasepool {
+                
+                print("Testing \(index) ...")
+                
+                let request = URLRequest(url: URL(string: link)!)
+                
+                let nativeLoadingResult = syncLoad(request: request)
+                
+                _ = syncCachingLoad(request: request)
+                
+                let cachingLoadingResult = syncCachingLoad(request: request)
+                
+                FBSnapshotCompareReferenceImage(nativeLoadingResult, to: cachingLoadingResult, tolerance: 0)
+            }
+        }
     }
     
-}
-
-extension NaiveCachingWebViewTests {
-    
-    func waitWebViewLoadingFinished() {
+    private class NavigationDelegate: NSObject, WKNavigationDelegate {
         
+        let dispatchGroup: DispatchGroup
+        
+        init(dispatchGroup: DispatchGroup) {
+            self.dispatchGroup = dispatchGroup
+        }
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.dispatchGroup.leave()
+            }
+        }
+    }
+
+    private func syncLoad(request: URLRequest) -> UIImage {
+        
+        let webView = WKWebView(frame: window.bounds)
+        let dispatchGroup = DispatchGroup()
+        let delegate = NavigationDelegate(dispatchGroup: dispatchGroup)
+        webView.navigationDelegate = delegate
+        window.addSubview(webView)
+
         dispatchGroup.enter()
+
+        _ = webView.load(request)
+        
         while dispatchGroup.wait(timeout: .now()) == .timedOut {
             RunLoop.main.run(until: Date() + 0.25)
         }
-    }
-}
-
-extension NaiveCachingWebViewTests: WKNavigationDelegate {
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.dispatchGroup.leave()
+        
+        guard let snapshot = image(forViewOrLayer: webView) else {
+            preconditionFailure("Failed to get the web view's snapshot.")
         }
+        
+        webView.navigationDelegate = nil
+        webView.removeFromSuperview()
+        
+        return snapshot
+    }
+    
+    private func syncCachingLoad(request: URLRequest) -> UIImage {
+        
+        let webView = WKWebView(frame: window.bounds)
+        let dispatchGroup = DispatchGroup()
+        let delegate = NavigationDelegate(dispatchGroup: dispatchGroup)
+        webView.navigationDelegate = delegate
+        window.addSubview(webView)
+        
+        dispatchGroup.enter() // wait navigation finished
+        dispatchGroup.enter() // wait caching finished
+        
+        _ = webView.cachingLoad(request, with: nil) {
+            dispatchGroup.leave()
+        }
+        
+        while dispatchGroup.wait(timeout: .now()) == .timedOut {
+            RunLoop.main.run(until: Date() + 0.25)
+        }
+        
+        guard let snapshot = image(forViewOrLayer: webView) else {
+            preconditionFailure("Failed to get the web view's snapshot.")
+        }
+        
+        webView.navigationDelegate = nil
+        webView.removeFromSuperview()
+        
+        return snapshot
+
     }
 }
