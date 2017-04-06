0f6bfaa6363a8e1bfd7769fed5a6596fcb2edaec
guoc <guochen42@gmail.com>
2017-04-06 23:05:14 +1000

    Add tests for cache

--- NaiveCachingWebViewTests/NaiveCachingWebViewTests.swift
+++ NaiveCachingWebViewTests/NaiveCachingWebViewTests.swift
@@ -65,6 +65,50 @@ class NaiveCachingWebViewTests: FBSnapshotTestCase {
         XCTAssert(WKWebView.hasCached(for: request))
     }
 
+// MARK: - Test cache
+
+    func testCache() {
+
+        let request = URLRequest(url: URL(string: "https://www.google.com")!)
+        syncCache(request: request)
+        syncCachingLoad(request: request)
+    }
+
+// MARK: - Test cache operations
+
+    func testCacheOperations() {
+
+        let urlStrings = [
+            "http://hackage.haskell.org/package/Agda-2.3.0/docs/src/Agda-TypeChecking-Monad-Env.html"
+          , "http://hackage.haskell.org/package/Agda-2.3.0/docs/Agda-TypeChecking-Forcing.html"
+          , "http://hackage.haskell.org/package/Agda-2.2.10/docs/Agda-TypeChecking-Errors.html"
+          , "http://hackage.haskell.org/package/Agda-2.2.8/docs/Agda-TypeChecking-Rules-Decl.html"
+          , "http://hackage.haskell.org/package/Agda-2.2.6/docs/src/Agda-Syntax-Parser-Layout.html"
+          , "http://hackage.haskell.org/package/Agda-2.2.6/docs/Agda-TypeChecking-Monad-Closure.html"
+          , "http://hackage.haskell.org/package/AERN-RnToRm-0.3.0.1/docs/Data-Number-ER-RnToRm-Approx.html"
+          , "http://hackage.haskell.org/package/AERN-Real-0.9.3/docs/Data-Number-ER-Real-Base.html"
+          , "http://hackage.haskell.org/package/ADPfusion-0.4.1.1/docs/ADP-Fusion-SynVar-Array-Type.html"
+          , "http://hackage.haskell.org/package/ADPfusion-0.4.0.1/docs/src/ADP-Fusion-TH-Common.html"
+        ]
+
+        let urls = urlStrings.map { URL(string: $0)! }
+        let requests = urls.map { URLRequest(url: $0) }
+        let cacheOperations = requests.map { WKWebView.cache($0, startAutomatically: false) }
+
+        let operationQueue = OperationQueue()
+        operationQueue.qualityOfService = .utility
+        operationQueue.addOperations(cacheOperations, waitUntilFinished: false)
+
+        while operationQueue.operationCount != 0 {
+            RunLoop.current.run(mode: .defaultRunLoopMode, before: .distantFuture)
+        }
+
+        let r = WKWebView.hasCached(for: requests[4])
+        print(r)
+
+        syncCachingLoad(request: requests[4])
+    }
+
 // MARK: - Test cancel cache operation
 
     func testCancelCacheOperation() {
@@ -164,7 +208,7 @@ class NaiveCachingWebViewTests: FBSnapshotTestCase {
         return snapshot
     }
     
-    private func syncCachingLoad(request: URLRequest) -> UIImage {
+    @discardableResult private func syncCachingLoad(request: URLRequest) -> UIImage {
         
         let webView = WKWebView(frame: window.bounds)
         let currentRunLoop = CFRunLoopGetCurrent()!
